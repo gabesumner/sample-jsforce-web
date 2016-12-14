32b44407cba81866f85e567c8a6932381c9d7623
Gabe Sumner <gsumner@gsumner-ltm6.internal.salesforce.com>
2016-12-14 14:51:07 -0800

    Fixed server. Changed ClientId.

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname));
 
-app.listen(app.get('port'), function() {
+app.listen(8080, function() {
   console.log('Node app is running on port', 8080);
 });
